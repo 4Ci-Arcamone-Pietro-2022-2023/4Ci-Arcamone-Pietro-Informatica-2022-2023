cddbd8f3abbb751cdf427d9dbd27d3d93474fcce
4CI-ArcamonePietro <117983250+4Ci-Arcamone-Pietro-2022-2023@users.noreply.github.com>
2022-12-16 23:47:10 +0100

    Create LavatriceTest.java

---
+++ 2022-2023/bin/lavatrice/VER01/LavatriceTest.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class LavatriceTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Lavatrice lava = new Lavatrice("bosch", "t3000", 200.0);
+        System.out.print(lava.toString());
+    }
+}
