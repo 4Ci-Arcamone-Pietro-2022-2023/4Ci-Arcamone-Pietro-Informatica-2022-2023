fc5bb883ba2f7335a5e3986f3b74cc90bd822df3
4CI-ArcamonePietro <117983250+4Ci-Arcamone-Pietro-2022-2023@users.noreply.github.com>
2022-12-17 00:33:18 +0100

    Create TelevisoreTest.java

---
+++ 2022-2023/bin/televisore/VER04/TelevisoreTest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class TelevisoreTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Televisore tv = new Televisore(27, "4k", "a colori");
+        System.out.print(tv.toString());
+        //02
+        tv.accendi();
+        System.out.print("\n"+ tv.toString());
+        tv.spegni();
+        System.out.print("\n"+ tv.toString());
+        //03
+        tv.setPollici(27);
+        System.out.print("\n\n"+ tv.getPollici());
+        //04
+        tv.setSchermo("4k");
+        System.out.print("\n\n"+ tv.getSchermo());
+    }
+}
