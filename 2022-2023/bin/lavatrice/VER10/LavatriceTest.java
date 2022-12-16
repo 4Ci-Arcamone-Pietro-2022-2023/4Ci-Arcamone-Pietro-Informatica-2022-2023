34416f9fb8ca66a35f9d5f966a0c1c330c9a37ad
4CI-ArcamonePietro <117983250+4Ci-Arcamone-Pietro-2022-2023@users.noreply.github.com>
2022-12-17 00:00:05 +0100

    Create LavatriceTest.java

---
+++ 2022-2023/bin/lavatrice/VER10/LavatriceTest.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class LavatriceTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Lavatrice lava = new Lavatrice("bosch", "t3000", 200.0);
+        System.out.print(lava.toString());
+        //02
+        lava.accendi();
+        System.out.print("\n" + lava.toString());
+        lava.spegni();
+        System.out.print("\n" + lava.toString());
+        //03
+        lava.setMarca("bosch");
+        System.out.print("\n" + lava.getMarca());
+        //04
+        lava.setModello("t3000");
+        System.out.print("\n" + lava.getModello());
+        //05
+        lava.setProgramma(2);
+        System.out.print("\n" + lava.getProgramma());
+        lava.aumentaProgramma();
+        System.out.print("\n" + lava.getProgramma());
+        lava.diminuisciProgramma();
+        System.out.print("\n" + lava.getProgramma());
+        //06
+        lava.setTemperatura(56);
+        System.out.print("\n" + lava.getTemperatura());
+        lava.aumentaTemperatura();
+        System.out.print("\n" + lava.getTemperatura());
+        lava.diminuisciTemperatura();
+        System.out.print("\n" + lava.getTemperatura());
+        //07
+        lava.setPrezzo(200.0);
+        System.out.print("\n" + lava.getPrezzo());
+        //08
+        lava.setGiri(500);
+        System.out.print("\n" + lava.getGiri());
+        lava.aumentaGiri();
+        System.out.print("\n" + lava.getGiri());
+        lava.diminuisciGiri();
+        System.out.print("\n" + lava.getGiri());
+        //09
+        lava.setKg(6);
+        System.out.print("\n" + lava.getKg());
+        //010
+        lava.setTempo(44);
+        System.out.print("\n" + lava.getTempo());
+        lava.aumentaTempo();
+        System.out.print("\n" + lava.getTempo());
+        lava.diminuisciTempo();
+        System.out.print("\n" + lava.getTempo());
+    }
+}
