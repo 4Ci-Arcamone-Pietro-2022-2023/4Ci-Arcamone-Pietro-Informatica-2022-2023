d5de2cc95433b3d6ade96c7ff9b0b17a4c4fb6e6
4CI-ArcamonePietro <117983250+4Ci-Arcamone-Pietro-2022-2023@users.noreply.github.com>
2022-12-17 00:59:05 +0100

    Create FrigoriferoTest.java

---
+++ 2022-2023/bin/frigorifero/VER03/FrigoriferoTest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class FrigoriferoTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Frigorifero frigo = new Frigorifero("samsung", "f2000", 700.0);
+        System.out.print("\n"+frigo.toString());
+        //02
+        frigo.accendi();
+        System.out.print("\n" + frigo.toString());
+        frigo.spegni();
+        System.out.print("\n" + frigo.toString());
+         //03
+        frigo.setMarca("samsung");
+        System.out.print("\n" + frigo.getMarca());
+    }
+}
